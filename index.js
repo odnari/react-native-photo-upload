0d90f5d49fe7394108caf59e5e4b78bffe730fca
Nick <nick@drakontas.com>
2019-01-30 12:26:47 +0200

    fix: pass image data directly

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ export default class PhotoUpload extends React.Component {
       })
 
       // handle photo in props functions as data string
-      if (this.props.onPhotoSelect) this.props.onPhotoSelect(photoData)
+      if (this.props.onPhotoSelect) this.props.onPhotoSelect(response.data)
     })
   }
 
